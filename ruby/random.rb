c21d210180ccac6a1fcb2abe13a96daf6eea824f
hie3-336 <tomohiko336@gmail.com>
2024-01-31 17:12:26 +0900

    グループ分けの人数をランダムで変更するように機能を追加

--- ruby/random.rb
+++ ruby/random.rb
@@ -1,5 +1,8 @@
-group_before = %w[a b c d e f]
-group_shuffle = group_before.shuffle
-a = 2
-p group_shuffle[0..a].sort
-p group_shuffle[a+1..5].sort
+GROUP_BEFORE = %w[a b c d e f]
+SPLIT_NUM = [3, 4]
+
+group_shuffle = GROUP_BEFORE.shuffle
+split = SPLIT_NUM.sample
+
+p group_shuffle[0..split - 1].sort
+p group_shuffle[split..5].sort
