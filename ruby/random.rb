375f0b3f432b81adef35a32b305b23e1efc6a5f2
hie3-336 <tomohiko336@gmail.com>
2024-01-31 17:15:20 +0900

    配列に使用されていたマジックナンバーを消す

--- ruby/random.rb
+++ ruby/random.rb
@@ -5,4 +5,4 @@ group_shuffle = GROUP_BEFORE.shuffle
 split = SPLIT_NUM.sample
 
 p group_shuffle[0..split - 1].sort
-p group_shuffle[split..5].sort
+p group_shuffle[split..GROUP_BEFORE.length - 1].sort
