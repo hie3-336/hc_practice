db9e84727fab1e54b96ffa222e995ce33862c10a
hie3-336 <tomohiko336@gmail.com>
2024-02-04 16:22:31 +0900

    ひとまず月末の日付を取得し、改行して日付を表示する部分まで作成

--- ruby/calendar.rb
+++ ruby/calendar.rb
@@ -1,3 +1,20 @@
 require 'date'
 today = Date.today
-print "     #{today.month}月 #{today.year} \n月 火 水 木 金 土 日"
+end_month = Date.new(today.year, today.month, -1).day
+puts end_month
+
+a = "     #{today.month}月 #{today.year} \n月 火 水 木 金 土 日\n"
+
+end_month.times do |i|
+  a << "#{i+1} ".rjust(3)
+  if i % 7 == 6 
+    a << "\n"
+  end
+end
+
+print a
+
+# b = " 1  2  3  4  5  6  7"
+# c = "\n"
+# d = " 8  9 10 11 12 13 14"
+# print a+b+c+d
