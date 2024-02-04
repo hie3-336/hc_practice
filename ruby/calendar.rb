efaa1ced4ba40e46d6da4257c86d91816eee12f6
hie3-336 <tomohiko336@gmail.com>
2024-02-04 16:47:14 +0900

    月初の曜日を取得し、正しい位置に1日が表示され改行されるように設定

--- ruby/calendar.rb
+++ ruby/calendar.rb
@@ -1,20 +1,20 @@
 require 'date'
 today = Date.today
 end_month = Date.new(today.year, today.month, -1).day
-puts end_month
+first_wday = (Date.new(today.year, today.month, 1).wday + 6) % 7
+puts first_wday
 
 a = "     #{today.month}月 #{today.year} \n月 火 水 木 金 土 日\n"
 
+first_wday.times do
+  a << "   "
+end
+
 end_month.times do |i|
   a << "#{i+1} ".rjust(3)
-  if i % 7 == 6 
+  if i % 7 == first_wday
     a << "\n"
   end
 end
 
 print a
-
-# b = " 1  2  3  4  5  6  7"
-# c = "\n"
-# d = " 8  9 10 11 12 13 14"
-# print a+b+c+d
