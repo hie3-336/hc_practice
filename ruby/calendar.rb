d6f5f9714b0bbe0c5de5c18104122b922c47b49b
hie3-336 <tomohiko336@gmail.com>
2024-02-06 23:28:43 +0900

    rjustが文字列出力の色変更を行なっていると効かないようなので、別方法でカレンダー出力

--- ruby/calendar.rb
+++ ruby/calendar.rb
@@ -6,8 +6,7 @@ opt = OptionParser.new
 params = {}
 opt.on('-m') {|v| params[:m] = v}
 opt.parse!(ARGV)
-# month = ARGV
-p params
+
 if params[:m]
   puts ARGV[0].to_i
 end
@@ -16,8 +15,7 @@ end
 today = Date.today
 end_month = Date.new(today.year, today.month, -1).day
 first_wday = (Date.new(today.year, today.month, 1).wday + 6) % 7
-test = "\e[30m\e[47mテスト\e[0m \n"
-print test
+# test = "\e[30m\e[47mテスト\e[0m \n"
 
 
 a = "     #{today.month}月 #{today.year} \n月 火 水 木 金 土 日\n"
@@ -27,7 +25,15 @@ first_wday.times do
 end
 
 end_month.times do |i|
-  a << "#{i+1} ".rjust(3)
+  if i < 9
+    a << " "
+  end
+  if !params[:m] && i == today.day - 1
+    a << "\e[30m\e[47m#{i+1}\e[0m ".rjust(3,"*")
+  else
+    a << "#{i+1} "
+  end
+  
   if i % 7 == first_wday
     a << "\n"
   end
