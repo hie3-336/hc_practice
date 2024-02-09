edd3da0b9b90363d3af54fe48d262273c8169410
hie3-336 <tomohiko336@gmail.com>
2024-02-10 00:58:19 +0900

    オプション、オプションエラーを受け付けるように設定

--- ruby/calendar.rb
+++ ruby/calendar.rb
@@ -7,18 +7,23 @@ params = {}
 opt.on('-m') {|v| params[:m] = v}
 opt.parse!(ARGV)
 
+today = Date.today
+
 if params[:m]
-  puts ARGV[0].to_i
+  if ARGV[0] =~ /^([1-9]|10|11|12)$/
+    c_month = ARGV[0].to_i
+  else
+    raise "#{ARGV[0]} is neither a month number (1..12) nor a name"
+  end
+else
+  c_month = today.month
 end
 
-
 today = Date.today
-end_month = Date.new(today.year, today.month, -1).day
-first_wday = (Date.new(today.year, today.month, 1).wday + 6) % 7
-# test = "\e[30m\e[47mテスト\e[0m \n"
+end_month = Date.new(today.year, c_month, -1).day
+first_wday = (Date.new(today.year, c_month, 1).wday + 6) % 7
 
-
-a = "     #{today.month}月 #{today.year} \n月 火 水 木 金 土 日\n"
+a = "     #{c_month}月 #{today.year} \n月 火 水 木 金 土 日\n"
 
 first_wday.times do
   a << "   "
@@ -29,12 +34,11 @@ end_month.times do |i|
     a << " "
   end
   if !params[:m] && i == today.day - 1
-    a << "\e[30m\e[47m#{i+1}\e[0m ".rjust(3,"*")
+    a << "\e[30m\e[47m#{i+1}\e[0m "
   else
     a << "#{i+1} "
   end
-  
-  if i % 7 == first_wday
+  if (first_wday + i) % 7 == 6
     a << "\n"
   end
 end
