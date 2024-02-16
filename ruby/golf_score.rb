c5aa8523fffe3db82b54fbac0f6f9d5dfc2d126d
hie3-336 <tomohiko336@gmail.com>
2024-02-17 02:39:18 +0900

    点数判定の機能としては完成(テストケース達成)

--- ruby/golf_score.rb
+++ ruby/golf_score.rb
@@ -3,7 +3,39 @@ p_scores = gets.split(',').map(&:to_i)
 
 raise "各ホールの規定打数とプレイヤーの打数の入力数が異なります" if r_scores.length != p_scores.length
 
-r_scores.each do |n|
-  score = r_scores[n] - p_scores[n]
-  p score
-end
+
+
+
+result = []
+r_scores.size.times {|n|
+  p n
+  score = p_scores[n] - r_scores[n] 
+  
+  # スコア差分だけで結果が求められない例外処理
+  if r_scores[n] == 5 && p_scores[n] == 1 
+    result << "コンドル"
+    next
+  elsif p_scores[n] == 1
+    result << "ホールインワン"
+    next
+  end
+
+  # puts score
+
+  case score
+  when -3
+    result << "アルバトロス"
+  when -2
+    result << "イーグル"
+  when -1
+    result << "バーディ"
+  when 0
+    result << "パー"
+  when 1
+    result << "ボギー"
+  else
+    result << "#{score}ボギー"
+  end
+}
+
+p result
