60c7a8395ef7a62d7779b111496dd378effc2f57
hie3-336 <tomohiko336@gmail.com>
2024-02-17 22:51:27 +0900

    rubocopの簡単な指摘に対処

--- ruby/golf_score.rb
+++ ruby/golf_score.rb
@@ -1,36 +1,37 @@
+# frozen_string_literal: true
+
 r_scores = gets.split(',').map(&:to_i)
 p_scores = gets.split(',').map(&:to_i)
 
-raise "各ホールの規定打数とプレイヤーの打数の入力数が異なります" if r_scores.length != p_scores.length
+raise '各ホールの規定打数とプレイヤーの打数の入力数が異なります' if r_scores.length != p_scores.length
 
 result = []
-r_scores.size.times {|n|
-  
+r_scores.size.times { |n|
   # スコア差分だけで結果が求められない例外処理
-  if r_scores[n] == 5 && p_scores[n] == 1 
-    result << "コンドル"
+  if r_scores[n] == 5 && p_scores[n] == 1
+    result << 'コンドル'
     next
   elsif p_scores[n] == 1
-    result << "ホールインワン"
+    result << 'ホールインワン'
     next
   end
 
-  score = p_scores[n] - r_scores[n] 
+  score = p_scores[n] - r_scores[n]
 
   case score
   when -3
-    result << "アルバトロス"
+    result << 'アルバトロス'
   when -2
-    result << "イーグル"
+    result << 'イーグル'
   when -1
-    result << "バーディ"
+    result << 'バーディ'
   when 0
-    result << "パー"
+    result << 'パー'
   when 1
-    result << "ボギー"
+    result << 'ボギー'
   else
     result << "#{score}ボギー"
   end
 }
 
-result.each{ |n| print "#{n}," }
+result.each { |n| print "#{n}," }
