97b2e79bee0267a6bae3ddb0567cae13b26dd0e9
hie3-336 <tomohiko336@gmail.com>
2024-03-24 23:21:29 +0900

    gitのミス修正

---
+++ vending_machine/juice.rb
@@ -0,0 +1,38 @@
+class Juice
+  ITEMS = [:pepsi, :monster, :ilohas]
+  PRICE = [150, 230, 120]
+  STOCK = [5, 5, 5]
+  SALES = 0
+
+  def initialize
+    @stock = STOCK
+    @sales = SALES
+  end
+
+  def check_stock(item)
+    item_no = ITEMS.index(item)
+    STOCK[item_no]
+  end
+
+  # @itemを外部から変更するメソッド
+  def suica=(value)
+    @suica = value
+  end
+  
+
+  def purchase(item)
+    item_no = ITEMS.index(item)
+    charge_amount = @suica.check_charge_amount
+
+    raise "You don't have enough money on your card." if charge_amount < PRICE[item_no]
+    raise "I'm sorry, this item is sold out." if @stock[item_no] <= 0
+
+    @stock[item_no] -= 1
+    @sales += PRICE[item_no]
+    @suica.spend(PRICE[item_no])
+  end
+
+  def check_sales
+    @sales
+  end
+end
---
+++ vending_machine/main.rb
@@ -0,0 +1,22 @@
+require_relative 'vending_machine'
+
+require_relative 'suica'
+require_relative 'juice'
+
+suica = Suica.new
+juice = Juice.new
+
+vending_machine = Vending_machine.new(:pepsi)
+vending_machine.item = juice
+
+# ↓????
+juice.suica = suica
+
+p vending_machine.check_stock
+
+p vending_machine.purchase
+p vending_machine.purchase
+p vending_machine.purchase
+
+
+p suica.check_charge_amount
