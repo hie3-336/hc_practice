750ba37da4689fa1555b76788b11f80f8617d400
hie3-336 <tomohiko336@gmail.com>
2024-03-31 15:56:54 +0900

    rubocopの指摘に対応

--- ruby/vending_machine/vending_machine.rb
+++ ruby/vending_machine/vending_machine.rb
@@ -3,17 +3,17 @@ require_relative 'juice'
 class VendingMachine
   def initialize
     @juices = [Juice.new(item: 'pepsi', price: 150, stock: 5),
-              Juice.new(item: 'monster', price: 230, stock: 5),
-              Juice.new(item: 'ilohas', price: 120, stock: 5)]
+               Juice.new(item: 'monster', price: 230, stock: 5),
+               Juice.new(item: 'ilohas', price: 120, stock: 5)]
     @sales = 0
   end
 
   # 複数の品物のうち、該当の品物のインスタンスを検索する処理
-  def find_item(i)
-    @juices.find{|n| n.check_item == i}
+  def find_item(item)
+    @juices.find { |n| n.check_item == item }
   end
 
-  # 品物のラインナップを返す処理(在庫が0の場合非表示) 
+  # 品物のラインナップを返す処理(在庫が0の場合非表示)
   def check_itemlist
     @juices.map do |juice|
       juice.check_item if juice.check_stock.positive?
@@ -61,5 +61,4 @@ class VendingMachine
   def sales
     @sales
   end
-
 end
