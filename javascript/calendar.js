8fc712a22a0c6bff99a6e4b0943de9874fa8ee22
hie3-336 <tomohiko336@gmail.com>
2025-04-12 23:17:08 +0900

    コメント追加

--- javascript/calendar.js
+++ javascript/calendar.js
@@ -1,6 +1,6 @@
-
 const nowDate = new Date();
 
+// 月の最終日を求める関数
 const getLastDay = (year, month) => {
   return new Date(year, month, 0).getDate();
 };
@@ -9,6 +9,7 @@ let year = nowDate.getFullYear();
 let month = nowDate.getMonth() + 1;
 let date = nowDate.getDate();
 
+// 引数エラー判定
 if (process.argv[2] === "-m") {
   month = process.argv[3];
   const monthPattern = new RegExp(/^([1-9]|10|11|12)$/);
@@ -23,14 +24,18 @@ if (process.argv[2] === "-m") {
   }
 }
 
+// 最初の2行を出力
 console.log (`     ${month}月 ${year}`);
 console.log (`日 月 火 水 木 金 土`);
 
 let d = 1;
-const firstWeekday = new Date(year, month - 1, 1).getDay();
 
+// 初日の曜日に合わせた空白出力
+const firstWeekday = new Date(year, month - 1, 1).getDay();
 let brank = '   '.repeat(firstWeekday);
 process.stdout.write(brank);
+
+// 残りの日にちを出力
 while (d <= getLastDay(year, month)) {
   let dayOutput = d < 10 ? ' ' + String(d) : String(d) ;
   if (process.argv[2] === undefined && d === date) {
@@ -40,6 +45,5 @@ while (d <= getLastDay(year, month)) {
   if (d % 7 === (7 - firstWeekday)) {
     console.log('');
   }
-
   d++;
 }
